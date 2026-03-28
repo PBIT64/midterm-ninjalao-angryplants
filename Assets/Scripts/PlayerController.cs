e98c1be2af41cd1472f5e6d21877ce1a52339cd5
PBIT64 <161491653+PBIT64@users.noreply.github.com>
2026-03-28 19:15:59 +0700

    fixedupdate for shooter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Rotate LeftRight
         if (Keyboard.current.aKey.isPressed)
